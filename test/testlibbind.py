f8c78175ff4e4f58db273a27ce2e2d14ff22d80d
Jason Smith <jhs@proven-corporation.com>
2005-02-14 16:51:34 +0700

    Moved the libbind tests into the correct testing file

--- test/testlibbind.py
+++ test/testlibbind.py
@@ -171,6 +171,9 @@ class libbindTestCase(unittest.TestCase):
 	assert(self.queries)
 	assert(self.responses)
 
+	self.packetData = file("data/oreilly.com-response").read()
+	self.msg = libbind.ns_msg(self.packetData)
+
 	self.flags = ['qr', 'opcode', 'aa', 'tc', 'rd', 'ra', 'z', 'ad', 'cd', 'rcode', 'max']
 	self.flags = map(lambda str: 'ns_f_' + str, self.flags)
 
@@ -219,7 +222,184 @@ class libbindTestCase(unittest.TestCase):
 		sectionVal = getattr(libbind, section)
 		self.assertEquals(libbind.ns_msg_count(msg, sectionVal), message['sections'][section])
 
-    def testReturnsRdata(self):
+    def testlibbind_ns_rr(self):
+	"""Test whether ns_rr_name returns the correct name"""
+
+	# Query
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_qd, 0)
+	assert(libbind.ns_rr_name(rr) == 'oreilly.com')
+
+	# Answer
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_an, 0)
+	assert(libbind.ns_rr_name(rr) == 'oreilly.com')
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_an, 1)
+	assert(libbind.ns_rr_name(rr) == 'oreilly.com')
+
+	# Name servers
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 0)
+	assert(libbind.ns_rr_name(rr) == 'oreilly.com')
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 1)
+	assert(libbind.ns_rr_name(rr) == 'oreilly.com')
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 2)
+	assert(libbind.ns_rr_name(rr) == 'oreilly.com')
+
+	# Additional
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 0)
+	assert(libbind.ns_rr_name(rr) == 'smtp1.oreilly.com')
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 1)
+	assert(libbind.ns_rr_name(rr) == 'smtp2.oreilly.com')
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 2)
+	assert(libbind.ns_rr_name(rr) == 'ns.oreilly.com')
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 3)
+	assert(libbind.ns_rr_name(rr) == 'ns1.sonic.net')
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 4)
+	assert(libbind.ns_rr_name(rr) == 'ns2.sonic.net')
+
+    def testlibbind_ns_rr_type(self):
+	"""Test whether ns_rr_type returns the correct type"""
+
+	# Query
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_qd, 0)
+	assert(libbind.ns_rr_type(rr) == libbind.ns_t_mx)
+
+	# Answer
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_an, 0)
+	assert(libbind.ns_rr_type(rr) == libbind.ns_t_mx)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_an, 1)
+	assert(libbind.ns_rr_type(rr) == libbind.ns_t_mx)
+
+	# Name servers
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 0)
+	assert(libbind.ns_rr_type(rr) == libbind.ns_t_ns)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 1)
+	assert(libbind.ns_rr_type(rr) == libbind.ns_t_ns)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 2)
+	assert(libbind.ns_rr_type(rr) == libbind.ns_t_ns)
+
+	# Additional
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 0)
+	assert(libbind.ns_rr_type(rr) == libbind.ns_t_a)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 1)
+	assert(libbind.ns_rr_type(rr) == libbind.ns_t_a)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 2)
+	assert(libbind.ns_rr_type(rr) == libbind.ns_t_a)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 3)
+	assert(libbind.ns_rr_type(rr) == libbind.ns_t_a)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 4)
+	assert(libbind.ns_rr_type(rr) == libbind.ns_t_a)
+
+    def testlibbind_ns_rr_class(self):
+	"""Test whether ns_rr_class returns the correct class"""
+
+	# This is only tested for inet class.  Chaosnet and Hesiod users will just have to wait.
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 0)
+	assert(libbind.ns_rr_class(rr) == libbind.ns_c_in)
+
+    def testlibbind_ns_rr_ttl(self):
+	"""Test whether ns_rr_ttl returns the correct TTL"""
+
+	# Query
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_qd, 0)
+	assert(libbind.ns_rr_ttl(rr) == 0)
+
+	# Answer
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_an, 0)
+	assert(libbind.ns_rr_ttl(rr) == 0xe10)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_an, 1)
+	assert(libbind.ns_rr_ttl(rr) == 0xe10)
+
+	# Name servers
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 0)
+	assert(libbind.ns_rr_ttl(rr) == 0x5460)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 1)
+	assert(libbind.ns_rr_ttl(rr) == 0x5460)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 2)
+	assert(libbind.ns_rr_ttl(rr) == 0x5460)
+
+	# Additional
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 0)
+	assert(libbind.ns_rr_ttl(rr) == 0x5460)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 1)
+	assert(libbind.ns_rr_ttl(rr) == 0x5460)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 2)
+	assert(libbind.ns_rr_ttl(rr) == 0x5460)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 3)
+	assert(libbind.ns_rr_ttl(rr) == 0x7f19)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 4)
+	assert(libbind.ns_rr_ttl(rr) == 0x7f19)
+
+    def testns_rr_rdlen(self):
+	"""Test whether ns_rr_rdlen returns the correct data length"""
+
+	# Query
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_qd, 0)
+	assert(libbind.ns_rr_rdlen(rr) == 0)
+
+	# Answer
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_an, 0)
+	assert(libbind.ns_rr_rdlen(rr) == 0x0a)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_an, 1)
+	assert(libbind.ns_rr_rdlen(rr) == 0x0a)
+
+	# Name servers
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 0)
+	assert(libbind.ns_rr_rdlen(rr) == 0x0f)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 1)
+	assert(libbind.ns_rr_rdlen(rr) == 0x06)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 2)
+	assert(libbind.ns_rr_rdlen(rr) == 0x05)
+
+	# Additional
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 0)
+	assert(libbind.ns_rr_rdlen(rr) == 4)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 1)
+	assert(libbind.ns_rr_rdlen(rr) == 4)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 2)
+	assert(libbind.ns_rr_rdlen(rr) == 4)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 3)
+	assert(libbind.ns_rr_rdlen(rr) == 4)
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 4)
+	assert(libbind.ns_rr_rdlen(rr) == 4)
+
+    def testlibbind_ns_rr_rdata(self):
+	"""Test whether ns_rr_rdata returns the correct data"""
+	import struct, socket
+	def ip2str(str):
+	    vals = str.split('.')
+	    vals = map(int, vals)
+	    return ''.join( map(chr, vals) )
+
+	# Query
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_qd, 0)
+	assert(libbind.ns_rr_rdata(rr) is None)
+
+	# Answer
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_an, 0)
+	assert(libbind.ns_rr_rdata(rr) == '\x00\x14\x05smtp1\xc0\x0c')
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_an, 1)
+	assert(libbind.ns_rr_rdata(rr) == '\x00\x14\x05smtp2\xc0\x0c')
+
+	# Name servers
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 0)
+	assert(libbind.ns_rr_rdata(rr) == '\x03ns1\x05sonic\x03net\00')
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 1)
+	assert(libbind.ns_rr_rdata(rr) == '\x03ns2\xc0\x59')
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ns, 2)
+	assert(libbind.ns_rr_rdata(rr) == '\x02ns\xc0\x0c')
+
+	# Additional
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 0)
+	assert(libbind.ns_rr_rdata(rr) == ip2str('209.204.146.22'))
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 1)
+	assert(libbind.ns_rr_rdata(rr) == ip2str('209.58.173.22'))
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 2)
+	assert(libbind.ns_rr_rdata(rr) == ip2str('209.204.146.21'))
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 3)
+	assert(libbind.ns_rr_rdata(rr) == ip2str('208.201.224.11'))
+	rr = libbind.ns_rr(self.msg, libbind.ns_s_ar, 4)
+	assert(libbind.ns_rr_rdata(rr) == ip2str('208.201.224.33'))
+
+    def testlibbind_ns_name_uncompress(self):
 	"""Test whether ns_name_uncompress returns the correct name"""
 	msg = libbind.ns_msg(self.responses[2]['data'])
 
